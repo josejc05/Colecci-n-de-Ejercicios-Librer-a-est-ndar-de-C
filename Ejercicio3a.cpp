a32b272638a136200468755b89b9593a6c14e198
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-18 12:51:58 +0100

    commit 5

---
+++ Ejercicio3a.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir.\n");
+        return 1;
+    }
+    fclose(file);
+    return 0;
+}
