6947247349432be7f6f8ef3b01930873ba6b101c
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-18 12:50:36 +0100

    commit 4

---
+++ Ejercicio2b.cpp
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    char str[] = "Hola mundo";
+    int length = strlen(str);
+    printf("La longitud de '%s' es: %d\n", str, length);
+    return 0;
+}
