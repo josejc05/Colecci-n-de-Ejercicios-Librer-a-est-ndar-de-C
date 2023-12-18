c8e9cc586e4db54fcaa193f7e99f19055219874c
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-18 12:44:32 +0100

    commit 1

---
+++ Ejercicio1a.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int num = -10;
+    int abs_num = abs(num);
+    printf("El valor absoluto de %d es %d\n", num, abs_num);
+    return 0;
+}
